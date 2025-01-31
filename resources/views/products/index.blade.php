8dcdab9aaa4fb55415f23100cc38330dd4b5519d
Michael White <hrt666.cs@gmail.com>
2025-01-31 16:27:34 +0900

    feat: added a link to create data in products/index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,5 +1,11 @@
 <h1>Products</h1>
 
+<a href="{{ route('products.create') }}">Create New Product</a> @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success')}}
+    </div>
+@endif
+
 <ul>
     @foreach ($products as $product)
         <li>
