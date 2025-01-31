65f493acd4cb0a485ae98a430ced8bea8dd5885a
Michael White <hrt666.cs@gmail.com>
2025-01-31 16:35:47 +0900

    feat: added create and store functions in app/http/Controllers/MyController.php

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -10,11 +10,29 @@ class MyController extends Controller
     public function index()
     {
         $products = Product::all();
-        return view('products.index', compact('products')); // Example view
+        return view('products.index', compact('products'));
     }
 
-            public function show(Product $product)
+    public function show(Product $product)
     {
-        return view('products.show',compact('product'));
+        return view('products.show', compact('product'));
+    }
+
+    public function create()
+    {
+        return view('products.create'); //Return the view for the form
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+        ]);
+
+        Product::create($request->all()); // Create the product (mass assignment is allowed because of $fillable)
+
+        return redirect()->route('products.index')->with('success', 'Product created successfully!'); // Redirect back to the product list
     }
 }
--- resources/views/show.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>{{$product->name}}</h1>
-<p>{{$product->description}}</p>
-<p>{{$product->price}}</p>
-<a href="/products">Back</a>
