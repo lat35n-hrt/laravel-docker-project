c53b5fdb72012203dc21b8d2b316142f20d23707
Michael White <hrt666.cs@gmail.com>
2025-02-02 20:15:13 +0900

    feat: added routes for edit and update in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,6 @@ Route::prefix('products')->group(function () {
     Route::get('/create', [MyController::class, 'create'])->name('products.create');
     Route::post('/', [MyController::class, 'store'])->name('products.store');
     Route::get('/{product}', [MyController::class, 'show'])->where('product', '[0-9]+')->name('products.show'); // ID is numeric
+    Route::get('/{product}/edit', [MyController::class, 'edit'])->where('product', '[0-9]+')->name('products.edit');
+    Route::put('/{product}', [MyController::class, 'update'])->where('product', '[0-9]+')->name('products.update');
 });
