a84502b0e11b68640f77ba5b19e890f4f58ba64e
Michael White <hrt666.cs@gmail.com>
2025-02-03 16:31:20 +0900

    feat: added delete feature in products/index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -15,6 +15,13 @@
             <td>{{ $product->price }}</td>
             <td><a href="/products/{{$product->id}}">Show</a></td>
             <td><a href="{{ route('products.edit', $product->id) }}">Edit</a></td>
+            <td>
+            <form action="{{ route('products.destroy', $product->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
+            </form>
+            </td>
         </tr>
     @endforeach
 
