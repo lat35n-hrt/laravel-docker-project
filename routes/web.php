97c9955bb82cd4950ef4eb24d0028c9618dc5c71
Michael White <hrt666.cs@gmail.com>
2025-02-03 16:26:48 +0900

    feat: added a route for delete(destroy) feature in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ Route::prefix('products')->group(function () {
     Route::get('/{product}', [MyController::class, 'show'])->where('product', '[0-9]+')->name('products.show'); // ID is numeric
     Route::get('/{product}/edit', [MyController::class, 'edit'])->where('product', '[0-9]+')->name('products.edit');
     Route::put('/{product}', [MyController::class, 'update'])->where('product', '[0-9]+')->name('products.update');
+    Route::delete('/{product}', [MyController::class, 'destroy'])->where('product', '[0-9]+')->name('products.destroy');
 });
