4a4d9f3a58c60bc33b5a5b605dea948a155e06a7
Michael White <hrt666.cs@gmail.com>
2025-02-05 18:00:28 +0900

    feat: Integrated Breeze and updated routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,28 @@
 <?php
+
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('products')->group(function () {
-    Route::get('/', [MyController::class, 'index'])->name('products.index');
-    Route::get('/create', [MyController::class, 'create'])->name('products.create');
-    Route::post('/', [MyController::class, 'store'])->name('products.store');
-    Route::get('/{product}', [MyController::class, 'show'])->where('product', '[0-9]+')->name('products.show'); // ID is numeric
-    Route::get('/{product}/edit', [MyController::class, 'edit'])->where('product', '[0-9]+')->name('products.edit');
-    Route::put('/{product}', [MyController::class, 'update'])->where('product', '[0-9]+')->name('products.update');
-    Route::delete('/{product}', [MyController::class, 'destroy'])->where('product', '[0-9]+')->name('products.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/products', [MyController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [MyController::class, 'create'])->name('products.create');
+    Route::post('/products', [MyController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}', [MyController::class, 'show'])->where('product', '[0-9]+')->name('products.show');
+    Route::get('/products/{product}/edit', [MyController::class, 'edit'])->where('product', '[0-9]+')->name('products.edit');
+    Route::put('/products/{product}', [MyController::class, 'update'])->where('product', '[0-9]+')->name('products.update');
+    Route::delete('/products/{product}', [MyController::class, 'destroy'])->where('product', '[0-9]+')->name('products.destroy');
 });
+
+require __DIR__.'/auth.php';
