c02eaa4c83f5dcdd3dd4436086406a79be87f377
Michael White <hrt666.cs@gmail.com>
2025-02-08 18:15:32 +0900

    feat: added permissions for update and delete to display links in resources/views/products/index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,8 @@
 <h1>Products</h1>
 
-<a href="{{ route('products.create') }}">Create New Product</a> @if (session('success'))
+<a href="{{ route('products.create') }}">Create New Product</a>
+
+@if (session('success'))
     <div class="alert alert-success">
         {{ session('success')}}
     </div>
@@ -14,7 +16,12 @@
             <td>{{ $product->description }}</td>
             <td>{{ $product->price }}</td>
             <td><a href="/products/{{$product->id}}">Show</a></td>
+
+            @can('update', $product)
             <td><a href="{{ route('products.edit', $product->id) }}">Edit</a></td>
+            @endcan
+
+            @can('delete', $product)
             <td>
             <form action="{{ route('products.destroy', $product->id) }}" method="POST">
                 @csrf
@@ -22,6 +29,8 @@
                 <button type="submit" onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
             </form>
             </td>
+            @endcan
+
         </tr>
     @endforeach
 
